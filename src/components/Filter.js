adda289cd26dc8d5a0cf72fc4c48fb99cd8b994b
Shawn Levin <shawnlevin22@gmail.com>
2018-04-23 22:48:45 -0700

    added filter to autocomplete input

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
 class Filter extends Component {
+
+
+
     render() {
         return (
             <div className="filter">
                 <img />
-                <input type="text" />
+                <input type="text" onKeyUp={event =>
+                    this.props.onTextChange(event.target.value)} />
                 Filter
         </div>
         )
--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -8,7 +8,7 @@ class Playlist extends Component {
         let playlist = this.props.playlists;
         return (
             <div className="playlists">
-                <img />
+                {/* <img /> */}
                 <h3>{playlist.name}</h3>
                 <ul>
                     {playlist.songs.map(song =>

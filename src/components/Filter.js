ac3ae1c331195e25a055d70bf3eb93276ff36134
Shawn Levin <shawnlevin22@gmail.com>
2018-04-23 18:14:16 -0700

    built components with fake server data

---
+++ src/components/Filter.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Filter extends Component {
+    render() {
+        return (
+            <div className="filter">
+                <img />
+                <input type="text" />
+                Filter
+        </div>
+        )
+    }
+}
+
+export default Filter;
---
+++ src/components/Playlist.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+
+
+class Playlist extends Component {
+    render() {
+        return (
+            <div className="playlists">
+                <img />
+                <h3>Playlist Name</h3>
+                <ul>
+                    <li>Song 1</li>
+                    <li>Song 2</li>
+                    <li>Song 3</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Playlist;

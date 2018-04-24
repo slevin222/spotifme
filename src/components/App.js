07d7ebfddfba09a77bd7423af3844706be1258d6
Shawn Levin <shawnlevin22@gmail.com>
2018-04-24 15:00:40 -0700

    fixed callback for heroku

--- src/components/App.js
+++ src/components/App.js
@@ -87,7 +87,7 @@ class App extends Component {
             </div> : <button onClick={() => {
               window.location = window.location.href.includes('localhost')
                 ? 'http://localhost:8888/login'
-                : 'https://better-playlists-backend.herokuapp.com/login'
+                : 'https://soptifme-backend.herokuapp.com/login'
             }
             }
               style={{ padding: '20px', 'fontSize': '50px', 'marginTop': '20px' }}>Sign in with Spotify</button>

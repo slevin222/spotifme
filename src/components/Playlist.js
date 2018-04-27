86d24b01e754db0820447408688dfb2565f77b85
Shawn Levin <shawnlevin22@gmail.com>
2018-04-26 23:26:14 -0700

    added YT search component

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
+
 
 
 
 
 class Playlist extends Component {
+
+
+    //example API search
+    // YTSearch({ key: API_KEY, term: 'surfboards' }, function (data) {
+    //     console.log(data);
+    // });
     render() {
         let playlist = this.props.playlists;
         return (
